e8db89877c37b9ec410a63b34458e3264fc5b203
LahiruMadushann <lahirumadushandl@gmail.com>
2026-01-03 12:06:34 +0530

    implement UserController

---
+++ backend/src/main/java/com/authsystem/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.authsystem.controller;
+
+import com.authsystem.dto.UserResponse;
+import com.authsystem.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getCurrentUser() {
+        return ResponseEntity.ok(userService.getCurrentUser());
+    }
+}
